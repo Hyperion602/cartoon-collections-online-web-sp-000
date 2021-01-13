9c7197f4950df78a77b385fe207c8f0aa448999b
James Harris <mharris9608@yahoo.com>
2021-01-13 19:01:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,24 +15,7 @@ end
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheeses.include?("cheddar") == true
-    def roll_call_dwarves(dwarves)
-    dwarves.each_with_index do |dwarf, index|
-      puts "#{index + 1} #{dwarf}"
-    end
-  end
-
-  def summon_captain_planet(elements)
-    elements.map {|ele| "#{ele.capitalize}!"}
-  end
-
-  def long_planeteer_calls(words)
-    words.any? { |word| word.length > 4}
-  end
-
-def find_the_cheese(cheeses)
-    cheese_types = ["cheddar", "gouda", "camembert"]
-    if cheeses.include?("cheddar") == true
-      return cheeses.find { |x| x == cheese_types}
-    else
+    return cheeses.find { |x| x == cheese_types}
+  else
   end
 end
