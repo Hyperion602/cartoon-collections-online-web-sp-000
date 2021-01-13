119a015d527a4f0f3f274acdc60e2c3b446214ba
James Harris <mharris9608@yahoo.com>
2021-01-13 19:01:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def find_the_cheese(cheeses)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if cheeses.include?("cheddar") == true
-    return cheeses.find { |x| x == cheese_types}
+    return cheeses.find { |x| x == "cheddar""}
   else
   end
 end
